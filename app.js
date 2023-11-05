9e4201d40d05622a4dc0536521d9d0be2f243d22
Ubuntu <ubuntu@ip-172-31-37-227.ec2.internal>
2023-11-05 01:49:34 +0000

    adding nodejs application

---
+++ app.js
@@ -0,0 +1,18 @@
+// This is a simple Node.js application that starts a server and listens on port 8080 for connections. 
+// The app responds with “Hello World!” for requests to the root URL (/) or route.
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
